b6335046f7b2e8bffc760e1de266cf5713df7419
Paweł Kałasiewicz <pawlus.kaasiewicz@gmail.com>
2018-05-24 20:28:54 +0200

    Przycisk

---
+++ src/wypozyczalnia/Okno.java
@@ -0,0 +1,15 @@
+
+package wypozyczalnia;
+
+import javax.swing.JFrame;
+
+public class Okno extends JFrame{
+    public Okno(){
+        super("Wypożyczalnia 1.0");
+        setSize(800,500);
+        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    
+}
--- src/wypozyczalnia/Wypozyczalnia.java
+++ src/wypozyczalnia/Wypozyczalnia.java
@@ -1,19 +1,18 @@
 package wypozyczalnia;
 
 import java.util.ArrayList;
-import java.util.List;
+
+
 
 
 public class Wypozyczalnia {
     
-    private ArrayList<Pojazd> listaPojazdow = new ArrayList<Pojazd>();
+    private static ArrayList<Pojazd> listaPojazdow = new ArrayList<Pojazd>();
 
     public static void main(String[] args) {
         Samochod mot1 = new Samochod(1, "Opel", "Astra", 2005, 5, "DW485WP", "SDFSDFS2323", 125, 1788, TypSamochodu.HATCHBACK, 5);
         
-       
-        
-    
+        Okno ramka = new Okno();
         
     
        
@@ -21,13 +20,12 @@ public class Wypozyczalnia {
         
     }
     
-    
+     
         
+    public static void wyswietl(){
         
-    public void wyswietl(){
-        
-            for (Pojazd listaPojazdow1 : listaPojazdow) {
-                System.out.println("fgh");
+            for (Pojazd i : listaPojazdow) {
+                System.out.println(i.getMarka());
             } 
         }
         
