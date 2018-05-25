c3e23de1d5756601fa8d10e4b17d7ad18a4430cd
Paweł Kałasiewicz <pawlus.kaasiewicz@gmail.com>
2018-05-25 22:14:53 +0200

    Zakladki

--- src/wypozyczalnia/Zakladki.java
+++ src/wypozyczalnia/Zakladki.java
@@ -2,9 +2,12 @@
 package wypozyczalnia;
 
 import com.sun.org.apache.bcel.internal.Constants;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import static javax.swing.SwingConstants.TOP;
 
 
 public class Zakladki extends JFrame implements Constants{
@@ -14,9 +17,11 @@ public class Zakladki extends JFrame implements Constants{
     private JTabbedPane zakladki = new JTabbedPane();
     
     public Zakladki(){
-        panelRodzic.setBounds(10,100,100,100);
-        zakladki.add("Samochody",panelSamochody);
+        panelRodzic.setBounds(10,100,600,300);
+        zakladki.add("Pojazdy",panelSamochody);
         zakladki.add("Klienci",panelKlienci);
+        zakladki.setTabPlacement(TOP);
+        panelRodzic.setLayout(new GridLayout());
         panelRodzic.add(zakladki);
     }
     
