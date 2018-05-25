e1935310ceffd8c34bfbb2501bf654a79cb70788
Paweł Kałasiewicz <pawlus.kaasiewicz@gmail.com>
2018-05-25 22:18:56 +0200

    Napis w zakladce

--- src/wypozyczalnia/Zakladki.java
+++ src/wypozyczalnia/Zakladki.java
@@ -5,6 +5,7 @@ import com.sun.org.apache.bcel.internal.Constants;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import static javax.swing.SwingConstants.TOP;
@@ -15,9 +16,14 @@ public class Zakladki extends JFrame implements Constants{
     private JPanel panelSamochody = new JPanel();
     private JPanel panelKlienci = new JPanel();
     private JTabbedPane zakladki = new JTabbedPane();
+    private JLabel lKlient = new JLabel("Klient!");
+    private JLabel lPojazdy = new JLabel("Pojaz!");
     
     public Zakladki(){
         panelRodzic.setBounds(10,100,600,300);
+        panelSamochody.add(lPojazdy);
+        panelKlienci.add(lKlient);
+        
         zakladki.add("Pojazdy",panelSamochody);
         zakladki.add("Klienci",panelKlienci);
         zakladki.setTabPlacement(TOP);
